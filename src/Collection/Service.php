61c8514c364930bf2baaf8a5f5c28bf574deeae8
Isaac <daprod2009@gmail.com>
2022-08-22 10:03:20 +0100

    cleanup service class for each product

--- src/Collection/Service.php
+++ src/Collection/Service.php
@@ -3,16 +3,4 @@ namespace Mediumart\MobileMoney\Collection;
 
 class Service extends Client
 {
-    /**
-     * Expose Client protected methods for now.
-     * The Service owned behaviors are to do next...
-     * 
-     * @param string $method
-     * @param array $arguments
-     * @return void
-     */
-    public function __call(string $method, array $arguments):void
-    {
-        $this->{$method}($arguments);
-    }
 }
