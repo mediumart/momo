09f15408e1c26aa10f47baa16965bc7f5ff91892
Isaac <daprod2009@gmail.com>
2022-09-05 11:28:42 +0100

    created transfer api trait for disbursement and remittance

--- src/Disbursement/Client.php
+++ src/Disbursement/Client.php
@@ -2,10 +2,13 @@
 namespace Mediumart\MobileMoney\Disbursement;
 
 use Mediumart\MobileMoney\BaseClient;
+use Mediumart\MobileMoney\TransferApi;
 use Psr\Http\Message\ResponseInterface;
 
 class Client extends BaseClient
 {
+    use TransferApi;
+    
     /**
      * Deposit an amount from the owner’s account to a payee account.
      * 
@@ -207,72 +210,6 @@ class Client extends BaseClient
     {
         return $this->client->request('GET',
             $this->baseurl.'/v1_0/refund/'.$requestId, [
-                'headers' => [
-                    'Authorization' => 'Bearer '.$token,
-                    'X-Target-Environment' => $targetEnv,
-                    'Ocp-Apim-Subscription-Key' => $subscriptionKey
-                ]
-            ]
-        );
-    }
-
-    /**
-     * Transfer an amount from the own account to a payee account.
-     * 
-     * @param string $subscriptionKey
-     * @param string $requestId
-     * @param string $token
-     * @param string $targetEnv
-     * @param array $payload
-     * @param string $callbackUrl
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function transfer(
-        string $subscriptionKey, 
-        string $requestId,
-        string $token,
-        string $targetEnv,
-        array $payload,
-        string $callbackUrl = null
-    ):ResponseInterface
-    {
-        $headers = [
-            'Authorization' => 'Bearer '.$token,
-            'X-Reference-Id' => $requestId,
-            'X-Target-Environment' => $targetEnv,
-            'Ocp-Apim-Subscription-Key' => $subscriptionKey
-        ];
-
-        if (! empty($callbackUrl)) {
-            $headers['X-Callback-Url'] = $callbackUrl;
-        }
-
-        return $this->client->request('POST',
-            $this->baseurl.'/v1_0/transfer', [
-                'headers' => $headers,
-                'body' => json_encode($payload)
-            ]
-        );
-    }
-
-    /**
-     * Get the status of a transfer.
-     * 
-     * @param string $subscriptionKey
-     * @param string $requestId
-     * @param string $token
-     * @param string $targetEnv
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function getTransferStatus(
-        string $subscriptionKey, 
-        string $requestId,
-        string $token,
-        string $targetEnv
-    ):ResponseInterface
-    {
-        return $this->client->request('GET',
-            $this->baseurl.'/v1_0/transfer/'.$requestId, [
                 'headers' => [
                     'Authorization' => 'Bearer '.$token,
                     'X-Target-Environment' => $targetEnv,
--- src/Remittance/Client.php
+++ src/Remittance/Client.php
@@ -2,73 +2,9 @@
 namespace Mediumart\MobileMoney\Remittance;
 
 use Mediumart\MobileMoney\BaseClient;
-use Psr\Http\Message\ResponseInterface;
+use Mediumart\MobileMoney\TransferApi;
 
 class Client extends BaseClient
 {
-    /**
-     * Transfer an amount from the own account to a payee account.
-     * 
-     * @param string $subscriptionKey
-     * @param string $requestId
-     * @param string $token
-     * @param string $targetEnv
-     * @param array $payload
-     * @param string $callbackUrl
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function transfer(
-        string $subscriptionKey, 
-        string $requestId,
-        string $token,
-        string $targetEnv,
-        array $payload,
-        string $callbackUrl = null
-    ):ResponseInterface
-    {
-        $headers = [
-            'Authorization' => 'Bearer '.$token,
-            'X-Reference-Id' => $requestId,
-            'X-Target-Environment' => $targetEnv,
-            'Ocp-Apim-Subscription-Key' => $subscriptionKey
-        ];
-
-        if (! empty($callbackUrl)) {
-            $headers['X-Callback-Url'] = $callbackUrl;
-        }
-
-        return $this->client->request('POST',
-            $this->baseurl.'/v1_0/transfer', [
-                'headers' => $headers,
-                'body' => json_encode($payload)
-            ]
-        );
-    }
-
-    /**
-     * Get the status of a transfer.
-     * 
-     * @param string $subscriptionKey
-     * @param string $requestId
-     * @param string $token
-     * @param string $targetEnv
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function getTransferStatus(
-        string $subscriptionKey, 
-        string $requestId,
-        string $token,
-        string $targetEnv
-    ):ResponseInterface
-    {
-        return $this->client->request('GET',
-            $this->baseurl.'/v1_0/transfer/'.$requestId, [
-                'headers' => [
-                    'Authorization' => 'Bearer '.$token,
-                    'X-Target-Environment' => $targetEnv,
-                    'Ocp-Apim-Subscription-Key' => $subscriptionKey
-                ]
-            ]
-        );
-    }
+    use TransferApi;
 }
