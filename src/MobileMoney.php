f56654921b3e3bc3f26b8a888f06d33a8022844a
Isaac <daprod2009@gmail.com>
2023-05-22 15:14:19 +0100

    wip

--- src/MobileMoney.php
+++ src/MobileMoney.php
@@ -1,14 +1,14 @@
 <?php
 namespace Mediumart\MobileMoney;
 
-/** 
- * @method static void setCurrentEnvironment()
+/**
+ * @method static void setCurrentEnvironment(string $value)
  * @method static string getCurrentEnvironment()
  * @method static Collection\Client collection()
  * @method static Disbursement\Client disbursement()
  * @method static Remittance\Client remittance()
  */
 class MobileMoney
-{   
+{
     use Factory;
 }
